d4ca0e3a4cb2c8f69ed4aac755ad294154f15c0b
Sanghyo Seo <125882656+seoshinehyo@users.noreply.github.com>
2025-01-06 11:44:03 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 172 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int by = sc.nextInt();
+        int sy = by - 543;
+        System.out.println(sy);
+    }
+}
