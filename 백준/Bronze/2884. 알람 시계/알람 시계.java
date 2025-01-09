85b68d1cbec8be2d33cf524cd982bcf70ad66aec
Sanghyo Seo <125882656+seoshinehyo@users.noreply.github.com>
2025-01-09 11:53:13 +0900

    [Bronze III] Title: 알람 시계, Time: 192 ms, Memory: 18520 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int h = sc.nextInt();
+        int m = sc.nextInt();
+        
+        if (m >= 45) {
+            System.out.println(h + " " + (m - 45));
+        } else {
+            if (h == 0) {
+                System.out.println("23 " + (60 - (45 - m)));
+            } else {
+                System.out.println((h - 1) + " " + (60 - (45 - m)));
+            }
+        }
+    }
+}
