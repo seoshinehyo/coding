a884b565a53620d863f2453942d4d12c17747a3d
Sanghyo Seo <125882656+seoshinehyo@users.noreply.github.com>
2025-01-11 15:29:10 +0900

    [Bronze V] Title: A+B - 4, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        String str;
+
+        while( (str=br.readLine()) != null ){
+
+            st = new StringTokenizer(str," ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a+b).append("\n");
+
+        }
+        System.out.print(sb);
+    }
+}
