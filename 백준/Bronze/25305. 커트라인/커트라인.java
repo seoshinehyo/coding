6893d33404b7bc27b4734eee9d41d64072fa9893
Sanghyo Seo <125882656+seoshinehyo@users.noreply.github.com>
2025-04-04 22:29:48 +0900

    [Bronze II] Title: 커트라인, Time: 224 ms, Memory: 20048 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int k = in.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = in.nextInt();
+        }
+        Arrays.sort(A);
+        System.out.println(A[N - k]);
+    }
+}
