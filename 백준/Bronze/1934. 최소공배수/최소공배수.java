81f4039765ce0b9f0f33b06c5428d91707b41124
Sanghyo Seo <125882656+seoshinehyo@users.noreply.github.com>
2025-05-02 22:23:08 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14604 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int d = gcd(a, b);
+			sb.append(a * b / d).append('\n');
+		}
+		System.out.println(sb);
+	}
+    
+	public static int gcd(int a, int b) {
+ 
+		while (b != 0) {
+			int r = a % b;
+			a = b;
+			b = r;
+		}
+		return a;
+	}
+}
